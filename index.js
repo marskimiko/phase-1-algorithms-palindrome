644f508957532635a29325f28ac01f46eedac101
marisasaunders <marisaksaunders@gmail.com>
2022-02-07 14:27:52 -0600

    commit tree b75f0de72a85341bbf3491166741bbdafb5d838b

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  
+  // convert word to array
+  const arrayWord = word.split('');
+
+  //reverse array
+  const reverseArrayWord = arrayWord.reverse();
+
+  // convert reversed array into a tring
+  const reverseString = reverseArrayWord.join('');
+
+  // compare reversed string to word and checks if they are equal
+  if (word === reverseString) {
+    return true;
+  } else {
+    return false; 
+  }
 }
 
 /* 
   Add your pseudocode here
+  - make if else statement
+  - compare if word = word.backwards and print true if it does
+  - return false if it does not
 */
 
 /*
